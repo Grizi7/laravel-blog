b4a930ce1d534622b305092b4380ca19f9023379
Grizi7 <mahmoudzidan7717@gmail.com>
2024-05-20 02:49:05 +0300

    integrating blog template into the project and dividing the home page into sections

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     if (Auth::user()->role == 'admin') {
