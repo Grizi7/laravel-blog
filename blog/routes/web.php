36b7e4c01a3145f4916942abd44a049c3ccbee74
Grizi7 <mahmoudzidan7717@gmail.com>
2024-05-19 06:46:48 +0300

    handling login logic for users and admins

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,14 +2,22 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/dashboard', function () {
+    if (Auth::user()->role == 'admin') {
+        return view('admin.dashboard');
+    }
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::get('/dashboard', function () {
+//     return view('admin.dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
