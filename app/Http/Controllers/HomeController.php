c906f07986ff2ec6c0392e4a7f40a6dcf6c5a180
Grizi7 <mahmoudzidan7717@gmail.com>
2024-05-23 17:30:33 +0300

    displaying posts in home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.index');
+        $posts = Post::latest()->limit(5)->get();
+        return view('home.index', [
+            'posts' => $posts
+        ]);
     }
 }
--- resources/views/home/layouts/services.blade.php
+++ resources/views/home/layouts/services.blade.php
@@ -1,21 +1,27 @@
 <div class="services_section layout_padding">
     <div class="container">
-        <h1 class="services_taital">Services </h1>
+        <h1 class="services_taital">Blog Posts</h1>
         <p class="services_text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration</p>
         <div class="services_section_2">
             <div class="row">
-                <div class="col-md-4">
-                    <div><img src="images/img-1.png" class="services_img"></div>
-                    <div class="btn_main"><a href="#">Rafting</a></div>
+                @foreach($posts as $post)
+                    <div class="col-md-4">
+                        <div><img src="{{ asset('storage/'. $post->image) }}" class="services_img rounded"></div>
+                        <p class="services_text text-muted text-justify mt-2 p-2">{{$post->content}}</p>
+                        <span class="text-secondary d-block mt-3 p-2">
+                            Added by <a href="http://localhost:8000/user/1" class="text-primary">Admin</a> on {{$post->created_at->diffForHumans()}}
+                        </span>
+                    <div class="btn_main"><a href="#">Read more</a></div>
                 </div>
-                <div class="col-md-4">
+                @endforeach
+                {{-- <div class="col-md-4">
                     <div><img src="images/img-2.png" class="services_img"></div>
                     <div class="btn_main active"><a href="#">Hiking</a></div>
                 </div>
                 <div class="col-md-4">
                     <div><img src="images/img-3.png" class="services_img"></div>
                     <div class="btn_main"><a href="#">Camping</a></div>
-                </div>
+                </div> --}}
             </div>
         </div>
     </div>
