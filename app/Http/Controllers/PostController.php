8aa6571ed7001d16ba70e12dadd481d85c03955a
Grizi7 <mahmoudzidan7717@gmail.com>
2024-05-27 16:48:02 +0300

    Update PostController.php to create add and store functions

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,4 +17,26 @@ class PostController extends Controller
             'post' => $post
         ]);
     }
+
+    public function add()
+    {
+        return view('home.add');
+    }
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title' => 'required|max:255|string',
+            'content' => 'required|string',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,webp|max:4096',
+        ]);
+        $data['user_id'] = auth()->id();
+        if ($request->file('image') != null) {
+            $data['image'] = $request->file('image')->store('images/posts');
+        } else {
+            $data['image'] = 'images/posts/default.png';
+        }
+        Post::create($data);
+
+        return redirect()->route('home')->with('success', 'Post created successfully');
+    }
 }
